89ac5fb9e324ffeaa313f7cf6734ef72342aba7c
Matt Styles <matt@veryfizzyjelly.com>
2015-05-21 12:16:16 +0100

    #add dirty open and close menu

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,5 +1,7 @@
 
 import React from 'react'
+import classNames from 'classnames'
+
 import appStore from '../stores/appStore'
 import Icon from 'icon/icon'
 
@@ -23,15 +25,52 @@ class MenuItem extends React.Component {
 class LoadItem extends React.Component {
     constructor( props ) {
         super( props )
+
+        this.state = {
+            open: false
+        }
+    }
+
+    onMoreClick( event ) {
+        this.setState({
+            open: !this.state.open
+        })
+    }
+
+    onLoadClick( event ) {
+        console.log( event.currentTarget )
+        
+        appStore.load()
     }
 
     render() {
+        let loadStates = <span></span>
+        if ( this.state.open ) {
+
+
+            loadStates = (
+                <ul className="Load-loadStates">
+                    <li className="Menu-action Menu-actionInner">
+                        <button className="Menu-actionButton Menu-actionInnerButton" onClick={ this.onLoadClick.bind( this ) }>From localstorage</button>
+                    </li>
+                </ul>
+            )
+        }
+
+        let classes = classNames({
+            'Menu-action Menu-load': true,
+            'Menu-load--isOpen': this.state.open
+        })
+
         return (
-            <li className="Menu-action Menu-load u-cf">
-                <button className="Menu-actionButton Menu-loadText" onClick={ this.props.onClick }>
-                    { this.props.text }
-                </button>
-                <button className="Menu-actionButton Menu-loadMore"><Icon icon="MORE" /></button>
+            <li className={ classes }>
+                <div className="Menu-loadMain u-cf">
+                    <button className="Menu-actionButton Menu-loadText" onClick={ this.props.onClick }>
+                        { this.props.text }
+                    </button>
+                    <button className="Menu-actionButton Menu-loadMore" onClick={ this.onMoreClick.bind( this ) }><Icon icon="MORE" /></button>
+                </div>
+                { loadStates }
             </li>
         )
     }
